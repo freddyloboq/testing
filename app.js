1fb4818509c0626b97f92da342870359f4ce5939
Freddy Lobo <freddyloboq@gmail.com>
2024-02-23 19:18:39 -0300

    testing

---
+++ app.js
@@ -0,0 +1,37 @@
+// const cars = ["Lambo", "Ferrari", "Pagani", "Audi", "BMW"];
+
+// let selectedCar = document.querySelector("select")
+// let label = document.querySelector("label")
+
+// for( let car of cars ){
+//   console.log('car', car)
+//   let option = document.createElement("OPTION")
+//   let text = document.createTextNode(car)
+//   // option.value = car
+
+//   option.appendChild(text)
+//   selectedCar.appendChild(option)
+
+//   // selectedCar.innerHTML += `<option value=${car}>${car}</option>`
+// }
+
+// label.addEventListener("mouseover", (event)=>{
+//   console.log("event", event);
+//   // console.log("seleccione el label")
+
+//   alert("selecciona otra cosa")
+// })
+
+// selectedCar.addEventListener("change", function(e){
+//   // console.log('.target.value', e.target.value)
+//   alert(`Este sera mi carro: ${e.target.value}`)
+// })
+
+// setTimeout(()=>{
+//   console.log('termina')
+//   selectedCar.removeEventListener("change", function (e) {});
+// }, 1000)
+
+
+console.log('hola')
+
